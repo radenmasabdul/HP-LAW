94caef8f9540ab2686831f00feeca7b69a9ebdf4
Raden Mas Abdul <mac@MacBook-Air.local>
2025-11-21 23:00:03 +0700

    Initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "./common/componentShadcn/theme/ModelToggle"
+
+function App() {
+  return (
+    <>
+    <ModeToggle/>
+    <h1 className="text-3xl font-bold underline text-black dark:text-white"> Hello world! </h1>    
+    </>
+  )
+}
+
+export default App
