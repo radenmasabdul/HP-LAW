5c709196ec1ff912c324b34dfaec0467a641b461
Raden Mas Abdul <mac@MacBook-Air.local>
2025-11-22 20:29:17 +0700

    Initialization

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
     <ModeToggle/>
     <div className="p-4">
-      <h1 className="text-3xl font-bold">{t("welcome")}</h1>
+      <h1 className="text-3xl font-bold fonts-inter">{t("welcome")}</h1>
       <button
         onClick={() => i18n.changeLanguage(i18n.language === "id" ? "en" : "id")}
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
